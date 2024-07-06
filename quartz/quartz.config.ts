af8a7b0e53bd1bc65513dffbe2660ec662ed4416
Nick Main <david.nick.main@gmail.com>
2024-07-06 08:40:53 -0700

    local fonts

--- quartz/quartz.config.ts
+++ quartz/quartz.config.ts
@@ -18,7 +18,7 @@ const config: QuartzConfig = {
     defaultDateType: "created",
     theme: {
       fontOrigin: "googleFonts",
-      cdnCaching: true,
+      cdnCaching: false,
       typography: {
         header: "Schibsted Grotesk",
         body: "Source Sans Pro",
