5b61ccf9d1ae7f2e9e07c33e54c26e698ce76f10
Nick Main <david.nick.main@gmail.com>
2024-07-07 12:47:12 -0700

    sadasads

--- quartz/quartz.config.ts
+++ quartz/quartz.config.ts
@@ -26,7 +26,7 @@ const config: QuartzConfig = {
       },
       colors: {
         lightMode: {
-          light: "#faf8f8",
+          light: "#f0f0ff",
           lightgray: "#e5e5e5",
           gray: "#b8b8b8",
           darkgray: "#4e4e4e",
@@ -40,7 +40,7 @@ const config: QuartzConfig = {
           lightgray: "#393639",
           gray: "#646464",
           darkgray: "#d4d4d4",
-          dark: "#ebebec",
+          dark: "#ebebff",
           secondary: "#7b97aa",
           tertiary: "#84a59d",
           highlight: "rgba(143, 159, 169, 0.15)",
